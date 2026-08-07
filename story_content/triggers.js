22f765bb495839f775d35b21af1e8c7070b84aa4
snceder <snceder@gmail.com>
2026-08-07 11:17:08 -0500

    Upload new course

---
+++ story_content/triggers.js
@@ -0,0 +1,65 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5lzDCDJOOZn":
+        Script1();
+        break;
+      case "5yJocVTOSRA":
+        Script2();
+        break;
+      case "5vfXKNUKUX2":
+        Script3();
+        break;
+      case "67HpufISd1Q":
+        Script4();
+        break;
+      case "5s6mCoJE5ui":
+        Script5();
+        break;
+      case "6bJE4wmdCk8":
+        Script6();
+        break;
+      case "6UekVC17IFI":
+        Script7();
+        break;
+      case "5iJGd3s5Ijj":
+        Script8();
+        break;
+      case "5tY7IRh5FNC":
+        Script9();
+        break;
+      case "6YgG0Ec8otA":
+        Script10();
+        break;
+      case "5hr9YBNNYgB":
+        Script11();
+        break;
+      case "6mrAqp5PFR8":
+        Script12();
+        break;
+      case "6T59LTGTrFh":
+        Script13();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var once = player.once;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+var update = player.update;
+var pointerX = player.pointerX;
+var pointerY = player.pointerY;
+var showPointer = player.showPointer;
+var hidePointer = player.hidePointer;
+var slideWidth = player.slideWidth;
+var slideHeight = player.slideHeight;
+var getKeyDown = player.getKeyDown;
+var keydown = player.keydown;
+var keyup = player.keyup;
+};
